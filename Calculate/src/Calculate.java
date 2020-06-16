4bccd455842b71ad89a83969c4b7b4baa5ecd2cf
Douglas Orsino <douglas.orsino123@outlook.com>
2020-06-16 01:54:14 -0300

    Nesta pasta estão os arquivos que criei em java em um simples teste de calculadora

---
+++ Calculate/src/Calculate.java
@@ -0,0 +1,28 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        if (args[0].equals("somar")) {
+            sum(x, y);
+        } else if (args[0].equals("subtrair")) {
+            sub(x, y);
+        } else if (args[0].equals("multiplicar")) {
+            mult(x, y);
+        } else if (args[0].equals("dividir")) {
+            divi(x, y);
+        }
+    }
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+    static void sub(int x, int y) {
+        System.out.println(x - y);
+    }
+    static void mult(int x, int y) {
+        System.out.println(x * y);
+    }
+    static void divi(int x, int y) {
+        System.out.println(x / y);
+    }
+}
